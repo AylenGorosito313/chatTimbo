e18b3118aa13fcfca30a7417c4c5c1e8ad97258a
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-02-04 13:37:48 -0300

    db env

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const server = require("./src/app");
 require("./src/models/coment");
 require("./src/models/users");
-
+const {DB_URI } = process.env;
 
 
 require("./src/models/User");
@@ -10,11 +11,11 @@ require("./src/models/Message");
 require("./src/models/Conversation");
 
 
-const uri = `mongodb+srv://kumani:agro123@cluster0.nkhpj6t.mongodb.net/test`;
+
 
 async function main() {
   mongoose.set('strictQuery',false)
-  await mongoose.connect(`${uri}`).then(()=>{console.log("MongoDB")})
+  await mongoose.connect(`${DB_URI}`).then(()=>{console.log("MongoDB")})
   server.listen(3001, async () => {
 
       console.log("server on port  3001  :) ");
